37acd7e8d0818c824b922b87e3c3ac0e0dce7319
NasrinSalika <nasrinsalika428@gmail.com>
2024-06-07 14:53:53 +0530

    vercel changes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const corsConfig = {
     credential: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 };
+app.options("", cors(corsConfig));
 app.use(cors(corsConfig));
 
 const app=express();
